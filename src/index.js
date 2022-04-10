1326811dcf469ce3b3876121cd777490d5eef128
SandersJ <sandersjack141@gmail.com>
2022-04-10 18:22:16 +0100

    added nav bar

--- src/index.js
+++ src/index.js
@@ -4,21 +4,62 @@ import Icon from './logo.png';
 //import Print from './print';
 
 function component() {
-  const element = document.createElement('div');
+  const Container = document.createElement('div');
+  Container.classList.add('container');
 
+  const Bar = document.createElement('div');
+  Bar.classList.add('Bar');
+
+  Container.appendChild(Bar)
+
+  const NavBar = document.createElement('div');
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  element.onclick = Print.bind(null, 'Hello webpack!');
+  //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  NavBar.classList.add('TopBar');
+   // Add the image to our existing div.;
+  //element.onclick = Print.bind(null, 'Hello webpack!');
 
- // Add the image to our existing div.
+  const Logo = document.createElement('l');
+  Logo.setAttribute('id','Logo');
+  NavBar.appendChild(Logo)
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  Logo.appendChild(myIcon);
 
- const myIcon = new Image();
- myIcon.src = Icon;
- element.appendChild(myIcon);
+  const Buttons = document.createElement('div');
+  Buttons.classList.add('Buttons');
 
+  const Home = document.createElement('a');
+  Home.innerHTML = 'Home'
+  Home.classList.add('active');
+  Home.setAttribute('id','Home');
+  Buttons.appendChild(Home);
 
-  return element;
+  const About = document.createElement('a');
+  About.innerHTML = 'About'
+  About.setAttribute('id','About');
+  Buttons.appendChild(About);
+
+  const SS = document.createElement('a');
+  SS.innerHTML = 'Success Stories'
+  SS.setAttribute('id','SS');
+  Buttons.appendChild(SS);
+
+  const Services = document.createElement('a');
+  Services.innerHTML = 'Services'
+  Services.setAttribute('id','Services');
+  Buttons.appendChild(Services);
+
+  const book = document.createElement('a');
+  book.innerHTML = 'Book Now'
+  book.setAttribute('id','Book');
+  Buttons.appendChild(book);
+
+  NavBar.appendChild(Buttons)
+  Bar.appendChild(NavBar)
+  Container.appendChild(Bar)
+
+  return Container;
 }
 
 document.body.appendChild(component());
